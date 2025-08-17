d81156022ed88b3db9bcad2b3933f5f617bf7695
dimmus <dmitri.chudinov@gmail.com>
2025-08-17 17:04:18 +0500

    Add option to disable building of demos

---
+++ clients/uil/UilMsgCatI.h
@@ -0,0 +1,106 @@
+/* $XConsortium$ */
+
+
+/* The following was generated from ./Uil.msg. */
+
+
+/* definitions for set UIL_SET1 */
+#define UIL_SET1 1
+
+#define UIL_0 1
+#define UIL_1 2
+#define UIL_2 3
+#define UIL_3 4
+#define UIL_4 5
+#define UIL_5 6
+#define UIL_6 7
+#define UIL_7 8
+#define UIL_8 9
+#define UIL_9 10
+#define UIL_10 11
+#define UIL_11 12
+#define UIL_12 13
+#define UIL_13 14
+#define UIL_14 15
+#define UIL_15 16
+#define UIL_16 17
+#define UIL_17 18
+#define UIL_18 19
+#define UIL_19 20
+#define UIL_20 21
+#define UIL_21 22
+#define UIL_22 23
+#define UIL_23 24
+#define UIL_24 25
+#define UIL_25 26
+#define UIL_26 27
+#define UIL_27 28
+#define UIL_28 29
+#define UIL_29 30
+#define UIL_30 31
+#define UIL_31 32
+#define UIL_32 33
+#define UIL_33 34
+#define UIL_34 35
+#define UIL_35 36
+#define UIL_36 37
+#define UIL_37 38
+#define UIL_38 39
+#define UIL_39 40
+#define UIL_40 41
+#define UIL_41 42
+#define UIL_42 43
+#define UIL_43 44
+#define UIL_44 45
+#define UIL_45 46
+#define UIL_46 47
+#define UIL_47 48
+#define UIL_48 49
+#define UIL_49 50
+#define UIL_50 51
+#define UIL_51 52
+#define UIL_52 53
+#define UIL_53 54
+#define UIL_54 55
+#define UIL_55 56
+#define UIL_56 57
+#define UIL_57 58
+#define UIL_58 59
+#define UIL_59 60
+#define UIL_60 61
+#define UIL_61 62
+#define UIL_62 63
+#define UIL_63 64
+#define UIL_64 65
+#define UIL_65 66
+#define UIL_66 67
+#define UIL_67 68
+#define UIL_68 69
+#define UIL_69 70
+#define UIL_70 71
+#define UIL_71 72
+#define UIL_72 73
+#define UIL_V2_73 74
+#define UIL_V2_74 75
+#define UIL_V2_75 76
+#define UIL_V2_76 77
+#define UIL_V2_77 78
+#define UIL_78 79
+#define UIL_V2_79 80
+#define UIL_V2_80 81
+
+/* definitions for set UIL_VALUES */
+#define UIL_VALUES 2
+
+#define UIL_VAL_0 1
+#define UIL_VAL_1 2
+
+/* definitions for set UIL_SET_MISC */
+#define UIL_SET_MISC 3
+
+#define UIL_INF 1
+#define UIL_WAR 2
+#define UIL_ERR 3
+#define UIL_SEV 4
+#define UIL_MISC_0 5
+#define UIL_MISC_1 6
