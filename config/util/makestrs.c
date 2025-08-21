c49e4067d847514d609566291c036fc90d1f9e37
dimmus <dmitri.chudinov@gmail.com>
2025-08-21 22:39:27 +0500

    Fix variable shadowing warnings in makestrs.c
    - Rename function parameters 'file' to 'phile' in three functions to avoid shadowing global static variable
    - Functions affected: IntelABIIndexEntries, DefaultIndexEntries, IndexEntries
    - Eliminates -Wshadow warnings during compilation
    - Maintains code functionality while improving code clarity

--- config/util/makestrs.c
+++ config/util/makestrs.c
@@ -604,12 +604,12 @@ DoLine(char* buf)
 }
 
 static void
-IntelABIIndexEntries (File* file)
+IntelABIIndexEntries (File* phile)
 {
     Table* t;
     TableEnt* te;
 
-    for (t = file->table; t; t = t->next)
+    for (t = phile->table; t; t = t->next)
 	for (te = t->tableent; te; te = te->next) {
 	    te->offset = t->offset;
 	    t->offset += strlen (te->right);
@@ -618,13 +618,13 @@ IntelABIIndexEntries (File* file)
 }
 
 static void
-DefaultIndexEntries (File* file)
+DefaultIndexEntries (File* phile)
 {
     Table* t;
     TableEnt* te;
     int offset = 0;
 
-    for (t = file->table; t; t = t->next)
+    for (t = phile->table; t; t = t->next)
 	for (te = t->tableent; te; te = te->next) {
 	    te->offset = offset;
 	    offset += strlen (te->right);
@@ -633,17 +633,17 @@ DefaultIndexEntries (File* file)
 }
 
 static void
-IndexEntries (File* file, int abi)
+IndexEntries (File* phile, int abi)
 {
     switch (abi) {
     case X_SPARC_ABI:
 	break;
     case X_INTEL_ABI:
     case X_INTEL_ABI_BC:
-	IntelABIIndexEntries (file);
+	IntelABIIndexEntries (phile);
 	break;
     default:
-	DefaultIndexEntries (file);
+	DefaultIndexEntries (phile);
 	break;
     }
 }
