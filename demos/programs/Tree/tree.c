830dd6e49bdd8740a30d5f2e0df8e422918e73d5
dimmus <dmitri.chudinov@gmail.com>
2025-08-28 19:30:17 +0500

    Tree demos: remove NeedFunctionPrototypes conditional blocks and modernize function declarations

--- demos/programs/Tree/tree.c
+++ demos/programs/Tree/tree.c
@@ -42,15 +42,9 @@
 /**************************************************************
 External functions
 **************************************************************/
-#ifdef NeedFunctionPrototypes
 #define ARGLIST(p)	(
 #define ARG(a, b)	a b,
 #define GRA(a, b)	a b)
-#else
-#define ARGLIST(p)	p
-#define ARG(a, b)	a b;
-#define GRA(a, b)	a b;
-#endif
 extern Pixmap XPM_PIXMAP
 ARGLIST((w, pixmapName))
 ARG(Widget, w)
